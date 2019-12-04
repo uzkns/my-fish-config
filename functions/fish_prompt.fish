069a77afbd215796a086f34b51dc77fd6dc39154
Max Goeckel <uzkns@student.kit.edu>
2019-12-04 14:05:25 +0100

    Added fish prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,18 +1,52 @@
 function fish_prompt --description 'Write out the prompt'
-	set -l color_cwd
-    set -l suffix
-    switch "$USER"
-        case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix '>'
+	
+    set stat $status
+
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
     end
 
-    echo -n -s "$USER" @ (prompt_hostname) ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
+    if not set -q __fish_color_blue
+        set -g __fish_color_blue (set_color -o blue)
+    end
+
+    if not set -q __git_cb
+        set __git_cb (set_color normal)" (:"(set_color brown)(git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)")"
+    end
+
+      # get the current dir nicely ... replace $HOME with "~"
+    set -l realhome ~
+    set -l __dir (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
+
+    #Set the color for the status depending on the value
+    set __fish_color_status (set_color -o green)
+    if test $stat -gt 0
+        set __fish_color_status (set_color -o red)
+    end
+
+    switch "$USER"
+
+        case root toor
+
+            if not set -q __fish_prompt_cwd
+                if set -q fish_color_cwd_root
+                    set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+                else
+                    set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+                end
+            end
+
+            printf '%s@%s %s%s%s# ' $USER (prompt_hostname) "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal"
+
+        case '*'
+
+            if not set -q __fish_prompt_cwd
+                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+            end
+
+            printf '[%s] %s%s%s %s(%s)%s \f\r∑ ' (date "+%H:%M:%S") "$__fish_prompt_cwd" "$__dir" "$__git_cb" "$__fish_color_status" "$stat" "$__fish_prompt_normal"
+
+    end
 end
+
+#cool symbols: ⋈ ∑ 
