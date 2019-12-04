6fcaa506fb8b5240983ea2229f7bb5fc71aadc80
Max Goeckel <uzkns@student.kit.edu>
2019-12-04 18:24:15 +0100

    Added distro info to welcome message, taken from /etc/os-release

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,4 +1,9 @@
 function fish_greeting --description 'Greeting when opening the console'
-    #Print user @ machine name (kernel version, arch)
-    printf '%s @ %s (%s %s, %s)' (whoami) (uname -n) (uname -o) (uname -r) (arch)
+    #get distro name and save it as a variable
+    if not set -q __os_name
+        set -g __os_name (cat /etc/os-release | grep NAME= | head -n 1 | cut -d= -f2 | tr -d '"')
+    end
+
+    #Print user @ machine name (distro arch, kernel version)
+    printf '%s@%s (%s %s, %s %s)\f\r' (whoami) (uname -n) "$__os_name" (arch) (uname -o) (uname -r)
 end
